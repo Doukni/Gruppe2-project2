c0222b940c825fb418d5be66a37aa0206a5190d7
abuyus04 <yoab0001@stud.kea.dk>
2025-03-31 17:27:33 +0200

    disdas

--- src/main/java/com/example/gruppe2project2/Gruppe2Project2Application.java
+++ src/main/java/com/example/gruppe2project2/Gruppe2Project2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Gruppe2Project2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Gruppe2Project2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Gruppe2Project2Application.class, args);
+    }
 
 }
